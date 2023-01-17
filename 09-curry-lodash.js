9bc2946ef0d62d24af3fb5b081b3425ff414c509
LxsZxh <1476273138@qq.com>
2023-01-17 19:35:41 +0800

    feat(09):模拟实现 lodash 中的 curry 方法

--- 09-curry-lodash.js
+++ 09-curry-lodash.js
@@ -8,3 +8,22 @@ const curried = _.curry(getSum);
 console.log(curried(1, 2, 3));
 console.log(curried(1)(2,3));
 console.log(curried(1)(2)(3)); 
+
+console.log('-------------------------------');
+
+// 模拟实现 lodash 中的 curry 方法
+
+function curry(func){
+  return function curriedFn(...args){
+    if(args.length < func.length){
+      return function(){
+        return curriedFn(...args.concat(Array.from(arguments)))
+      }
+    }
+    return func(...args)
+  }
+}
+const curried2= curry(getSum);
+console.log(curried2(1, 2, 3));
+console.log(curried2(1)(2,3));
+console.log(curried2(1)(2)(3)); 
