b73d820ac8aa7602b732afa635cdef30d16aa476
LxsZxh <1476273138@qq.com>
2023-01-18 14:59:49 +0800

    feat(12):手写lodash 中的flowRight()

--- 12-compose-lodash.js
+++ 12-compose-lodash.js
@@ -7,3 +7,27 @@ const toUpper = (str) => str.toUpperCase();
 
 const res = _.flowRight(toUpper, first, reverse);
 console.log(res(["Hello", "World"]));
+
+console.log("-------------------------------");
+
+// 手写 flowRight 函数
+function compose(...args) {
+  return function (value) {
+    return args.reverse().reduce(function (acc, fn) {
+      return fn(acc);
+    }, value);
+  };
+}
+const composeRes = compose(toUpper, first, reverse);
+console.log(composeRes(["Hello", "World"]));
+
+console.log("-------------------------------");
+
+// 箭头函数书写 flowRight 函数
+const arrowCompose =
+  (...args) =>
+  (value) =>
+    args.reverse().reduce((acc, fn) => fn(acc), value);
+
+const arrowRes = arrowCompose(toUpper, first, reverse);
+console.log(arrowRes(["Hello", "World"]));
