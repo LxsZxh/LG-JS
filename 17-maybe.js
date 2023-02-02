641f4550b7a4a28e7f37a99168c6724a009b221f
LxsZxh <1476273138@qq.com>
2023-02-02 16:17:03 +0800

    feat(17):maybe函子

---
+++ 17-maybe.js
@@ -0,0 +1,34 @@
+// Maybe 函子
+class Maybe {
+  static of(value) {
+    return new Maybe(value);
+  }
+
+  constructor(value) {
+    this._value = value;
+  }
+
+  map(fn) {
+    return this.isNothing() ? Maybe.of(null) : Maybe.of(fn(this._value));
+  }
+  isNothing() {
+    return this._value === null || this._value === undefined;
+  }
+}
+
+const res1 = Maybe.of(5)
+  .map((item) => item + 1)
+  .map((item) => item * item);
+console.log(res1);
+
+console.log("---------------------");
+
+const res2 = Maybe.of(null)
+  .map((item) => item + 1)
+  .map((item) => item * item);
+console.log(res2);
+
+console.log("---------------------");
+
+const res3 = Maybe.of("Hello World").map((item) => item.toUpperCase());
+console.log(res3);
